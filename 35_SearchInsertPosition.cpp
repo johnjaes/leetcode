8f68aa605731130fe826a17eb13a5acd86b2cdc1
johnjaes <46451915+johnjaes@users.noreply.github.com>
2019-01-08 03:47:33 +0800

    Create 35_SearchInsertPosition.cpp

---
+++ 35_SearchInsertPosition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            if(nums[i] == target) { return i; }
+            else if(nums[i] > target && i == 0){ return i; }
+            else if(nums[i] < target && i == nums.size() - 1){ return i+1;}
+            else if(nums[i] < target && nums[i+1] > target) { return i+1; }
+            
+        }
+    }
+};
