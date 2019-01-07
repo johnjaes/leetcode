fcddefae88eed5dd18dea0dcc60f0bfacf0860d6
johnjaes <46451915+johnjaes@users.noreply.github.com>
2019-01-08 04:05:07 +0800

    Create 137_SingleNumberII.cpp

---
+++ 137_SingleNumberII.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i] > nums[j])
+				{
+					int temp = nums[i];
+					nums[i] = nums[j];
+					nums[j] = temp;
+				}
+			}
+		}
+		for (int co = 0; co < nums.size(); co+=3)
+		{
+			if (co == nums.size() - 1) { return nums[co]; }
+			if (nums[co] != nums[co + 1] || nums[co] != nums[co + 2]) { return nums[co]; }
+		}
+    }
+};
