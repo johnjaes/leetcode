6205f29917edf24757c0501a6edf91b1c0f82022
U0 <46451915+johnjaes@users.noreply.github.com>
2019-03-26 23:28:50 +0800

    Create 905_SortArrayByParity.cpp

---
+++ 905_SortArrayByParity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        vector<int> ans;
+        for(int i = 0 ; i < A.size() ; i++)
+        {
+            if(A[i]%2 == 0)
+            {
+                ans.insert(ans.begin() , A[i]);
+            }
+            else
+            {
+                ans.push_back(A[i]);
+            }
+        }
+        return ans;
+    }
+};
